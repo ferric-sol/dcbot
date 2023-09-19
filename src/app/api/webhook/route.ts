f29640a59c0505498ab110ae00b72c85e4c46db4
ferric <ferric-sol@ferric.net>
2023-09-20 00:46:59 +0530

    Use web3.js

--- src/app/api/webhook/route.ts
+++ src/app/api/webhook/route.ts
@@ -1,16 +1,41 @@
 import { NextResponse } from 'next/server';
- 
-export async function GET(request: Request) {
+import express from 'express';
+import Web3 from 'web3';
+
+const app = express();
+const web3 = new Web3('https://eth-mainnet.g.alchemy.com/v2/YN54Eaz5Mz5wsaGdqmEYjc2RGAExwREZ');
+
+export async function GET(request: any) {
   const { searchParams } = new URL(request.url);
-  const hasTitle = searchParams.has('title');
-  const title = hasTitle
-    ? searchParams.get('title')?.slice(0, 100)
-    : 'My default title';
- 
-  return NextResponse.json(
-    { title },
-    {
-      status: 200,
-    },
-  );
+  const hasAddress = searchParams.has('address');
+  const ethAddress = hasAddress ? searchParams.get('address') : null;
+
+  if (!ethAddress || !web3.utils.isAddress(ethAddress)) {
+    return NextResponse.json(
+      { error: 'Invalid Ethereum address' },
+      {
+        status: 400,
+      }
+    );
+  }
+
+  try {
+    const balanceWei = await web3.eth.getBalance(ethAddress);
+    const balanceEth = web3.utils.fromWei(balanceWei, 'ether');
+
+    return NextResponse.json(
+      { balanceWei, balanceEth },
+      {
+        status: 200,
+      }
+    );
+  } catch (error) {
+    console.error(error);
+    return NextResponse.json(
+      { error: 'Server error' },
+      {
+        status: 500,
+      }
+    );
+  }
 }
