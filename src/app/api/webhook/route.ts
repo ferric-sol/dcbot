5aaedf957d23c5878011283635d5142b5db44d9f
ferric <ferric-sol@ferric.net>
2023-09-21 03:44:14 +0530

    Add generate

--- src/app/api/webhook/route.ts
+++ src/app/api/webhook/route.ts
@@ -1,78 +1,92 @@
 import { NextResponse } from 'next/server';
-import express from 'express';
-import Web3, { eth } from 'web3';
+import Web3 from 'web3';
 import TelegramBot from 'node-telegram-bot-api';
-
-const app = express();
+import { createClient } from '@vercel/kv';
 
 const web3 = new Web3('https://eth-mainnet.g.alchemy.com/v2/YN54Eaz5Mz5wsaGdqmEYjc2RGAExwREZ');
 const bot = new TelegramBot('6341931544:AAEH6yep5M6mkSCto0WQSnK_IzoXaI-hMGw');
 
-export async function POST(request: Request) {
-  const body = await request.json();
-  const { chat: { id }, text } = body.message;
+const kv = createClient({
+  url: process.env.KV_REST_API_URL,
+  token: process.env.KV_REST_API_TOKEN,
+});
 
-  let ethAddressOrEns = text;
-  ethAddressOrEns = ethAddressOrEns.replace('@devconnect_griffith_bot ', '');
+function sendErrorResponse() {
+  return NextResponse.json(
+    { error: 'Invalid Ethereum address' },
+    {
+      status: 200,
+    }
+  );
+}
 
+async function handleCommand(id, text, username) {
+  let ethAddressOrEns = text.replace('@devconnect_griffith_bot ', '').trim();
   let ethAddress = null;
 
-  if(ethAddressOrEns.startsWith('/')) {
-    if(ethAddressOrEns.startsWith('/balanceaddr')) {
-      ethAddressOrEns = ethAddressOrEns.replace('/balanceaddr', '');
-      ethAddressOrEns = ethAddressOrEns.trim();
-      if(!ethAddressOrEns) {
-        return NextResponse.json(
-          { error: 'Invalid Ethereum address' },
-          {
-            status: 200,
-          }
-        );
+  switch (true) {
+    case ethAddressOrEns.startsWith('/balanceaddr'):
+      ethAddressOrEns = ethAddressOrEns.replace('/balanceaddr', '').trim();
+      if (!ethAddressOrEns) {
+        return sendErrorResponse();
       }
       ethAddress = ethAddressOrEns;
-    } else if(ethAddressOrEns.startsWith('/balance')) {
-      ethAddressOrEns = ethAddressOrEns.replace('/balance', '');
-      ethAddressOrEns = ethAddressOrEns.trim();
-      if(ethAddressOrEns.length <= 0) {
+      break;
+
+    case ethAddressOrEns.startsWith('/balance'):
+      ethAddressOrEns = ethAddressOrEns.replace('/balance', '').trim();
+      if (ethAddressOrEns.length <= 0) {
+        return sendErrorResponse();
+      }
+      ethAddress = Buffer.from(await web3.eth.ens.getAddress(ethAddressOrEns)).toString();
+      break;
+
+    case ethAddressOrEns.startsWith('/generate'):
+      const account = web3.eth.accounts.create();
+      const keyPair = {
+        address: account.address,
+        privateKey: account.privateKey,
+      };
+
+      try {
+        await kv.set(`user:${username}`, JSON.stringify(keyPair), { ex: 100, nx: true });
+      } catch (error) {
+        console.error('Error storing the key pair:', error);
         return NextResponse.json(
-          { error: 'Invalid Ethereum address' },
+          { error: 'Error storing the key pair' },
           {
-            status: 200,
+            status: 500,
           }
         );
       }
-      ethAddress = Buffer.from(await web3.eth.ens.getAddress(ethAddressOrEns)).toString();
-    } else {
+
+      await bot.sendMessage(id, `✅ Key pair generated successfully:\n- Address: ${keyPair.address}\n- Private Key: ${keyPair.privateKey}`, { parse_mode: 'Markdown' });
       return NextResponse.json(
-        { error: 'Invalid Ethereum address' },
+        { keyPair },
         {
           status: 200,
         }
       );
-    }
-  } else {
-    ethAddress = Buffer.from(await web3.eth.ens.getAddress(ethAddressOrEns)).toString();
+      break;
+
+    default:
+      return sendErrorResponse();
   }
 
   if (!ethAddress || !web3.utils.isAddress(ethAddress)) {
     const message = 'Address not understood';
-    await bot.sendMessage(id, message, {parse_mode: 'Markdown'});
-    return NextResponse.json(
-      { error: 'Invalid Ethereum address' },
-      {
-        status: 200,
-      }
-    );
+    await bot.sendMessage(id, message, { parse_mode: 'Markdown' });
+    return sendErrorResponse();
   }
 
   try {
     const balanceWei = await web3.eth.getBalance(ethAddress);
     const balanceEth = web3.utils.fromWei(balanceWei, 'ether');
-
-    // Convert balanceWei to a regular number (if it's safe to do so)
+    
     const balanceWeiNumber = Number(balanceWei);
-    const message = `✅ The balance for address: *"${ethAddress}"* is ${balanceEth}\nHave a great day! 👋🏻`;
-    await bot.sendMessage(id, message, {parse_mode: 'Markdown'});
+    const message = `✅ The balance for address: *"${ethAddress}"* is ${balanceEth} ETH\nHave a great day! 👋🏻`;
+    await bot.sendMessage(id, message, { parse_mode: 'Markdown' });
+
     if (Number.isSafeInteger(balanceWeiNumber)) {
       return NextResponse.json(
         { balanceWei: balanceWeiNumber, balanceEth },
@@ -81,23 +95,28 @@ export async function POST(request: Request) {
         }
       );
     } else {
-      // Handle the case where the balanceWei is too large for Number
       return NextResponse.json(
         { 
-          message:  'Balance is too large to convert to a number',
+          message: 'Balance is too large to convert to a number',
           status: 200,
         }
       );
     }
   } catch (error) {
     console.error(error);
-    const message = 'Address not understood';
-    await bot.sendMessage(id, message, {parse_mode: 'Markdown'});
-    return NextResponse.json(
-      { error: 'Invalid Ethereum address' },
-      {
-        status: 200,
-      }
-    );
+    const message = 'Error fetching balance';
+    await bot.sendMessage(id, message, { parse_mode: 'Markdown' });
+    return sendErrorResponse();
   }
+}
+
+export async function POST(request) {
+  const body = await request.json();
+  const { chat: { id }, text, entities, message: { from: { username } } } = body;
+
+  if(entities && entities[0].type === 'bot_command') {
+    return handleCommand(id, text, username);
+  }
+
+  // ... (other logic for POST function, if any)
 }
